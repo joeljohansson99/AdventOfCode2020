99dc069d5f06a1be4762cade78910a9b39c9f1b0
joel.johansson <joel.k.johansson@gmail.com>
2020-12-03 06:41:04 +0100

    Day 3

--- src/solutions/Day3.java
+++ src/solutions/Day3.java
@@ -14,14 +14,32 @@ public class Day3 implements Day{
 	public String part1() {
 		// List<String> strList = input.getList();
 		// String[] strArr = input.getArray();
-		return null;
+		
+		return String.valueOf(countPath(1,3));
 	}
 
 	@Override
 	public String part2() {
 		// List<String> strList = input.getList();
 		// String[] strArr = input.getArray();
-		return null;
+		return String.valueOf(countPath(1,1)*countPath(1,3)*countPath(1,5)*countPath(1,7)*countPath(2,1));
+	}
+	
+	long countPath(int r, int c) {
+		String[] strArr = input.getArray();
+		int count = 0;
+		int k = 0;
+		for (int i = r; i < strArr.length; i+=r) {
+			k += c;
+
+			if (strArr[i].length() <= k) {
+				k = k - strArr[i].length();
+			}
+			if (strArr[i].charAt(k) == '#') {
+				count++;
+			}
+		}
+		return count;
 	}
 
 }
