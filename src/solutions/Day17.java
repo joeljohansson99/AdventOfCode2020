3864b30a0859e6b20abc8844afeac735b6f89607
joel.johansson <joel.k.johansson@gmail.com>
2020-12-16 14:48:39 +0100

    Day 16

---
+++ src/solutions/Day17.java
@@ -0,0 +1,28 @@
+package solutions;
+
+import java.util.*;
+import utils.*;
+
+public class Day17 implements Day{
+	
+	Input input;
+	
+	public Day17() {
+		this.input = new Input("/home/joel/eclipse-workspace/AdventOfCode/input/day17.txt");
+	}
+
+	@Override
+	public String part1() {
+		// List<String> strList = input.getList();
+		// String[] strArr = input.getArray();
+		
+		return null;
+	}
+	
+	public String part2() {
+		// List<String> strList = input.getList();
+		//String[] strArr = input.getArray();
+		
+		return null;
+	}
+}
